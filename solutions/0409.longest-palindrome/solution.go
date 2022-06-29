5bcb9cfde6f86b4ae557196bdcfe8d574090f461
y573724537 <y573724537@163.com>
2022-06-29 23:27:22 +0800

    add 409

---
+++ solutions/0409.longest-palindrome/solution.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func longestPalindrome(s string) int {
+	letterMap := make(map[byte]int)
+	for i := 0; i < len(s); i++ {
+		letterMap[s[i]]++
+	}
+
+	ans := 0
+	for _, num := range letterMap {
+		ans += num / 2 * 2
+		if ans%2 == 0 && num%2 == 1 {
+			ans++
+		}
+	}
+
+	return ans
+}
+
+func main() {
+	fmt.Printf("%v\n", longestPalindrome("civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth"))
+}
